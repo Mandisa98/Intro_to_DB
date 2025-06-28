7baa21eb55d22519c5691a53098f4d18f674dff0
Mandisa Mabena <ndindi.mm@gmail.com>
2025-06-28 17:42:49 +0200

    Add exception handling to MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -14,8 +14,8 @@ try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
-    print(f"Error while connecting to MySQL: {e}")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
 
 finally:
     # Always close the connection to avoid memory leaks
